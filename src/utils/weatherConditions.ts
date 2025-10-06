ff7a832afe722d8f12d5e1654e498cfdc03bba8a
Mallikarjuna74 <164886670+Mallikarjuna74@users.noreply.github.com>
2025-10-07 00:05:47 +0530

    updated new file path to all img

--- src/utils/weatherConditions.ts
+++ src/utils/weatherConditions.ts
@@ -2,16 +2,16 @@ export const weatherConditions: Record<
   number,
   { icon: string; label: string }
 > = {
-  0: { icon: "/images/icon-sunny.webp", label: "Clear sky" },
-  1: { icon: "/images/icon-sunny.webp", label: "Mainly clear" },
-  2: { icon: "/images/icon-partly-cloudy.webp", label: "Partly cloudy" },
-  3: { icon: "/images/icon-overcast.webp", label: "Overcast" },
-  45: { icon: "/images/icon-fog.webp", label: "Fog" },
-  48: { icon: "/images/icon-fog.webp", label: "Depositing rime fog" },
-  51: { icon: "src/assets/images/icon-rain.webp", label: "Light drizzle" },
-  61: { icon: "src/assets/images/icon-rain.webp", label: "Rain" },
-  71: { icon: "src/assets/images/icon-snow.webp", label: "Snow" },
-  95: { icon: "src/assets/images/icon-storm.webp", label: "Thunderstorm" },
+  0: { icon: "/Weather-website/images/icon-sunny.webp", label: "Clear sky" },
+  1: { icon: "/Weather-website/images/icon-sunny.webp", label: "Mainly clear" },
+  2: { icon: "/Weather-website/images/icon-partly-cloudy.webp", label: "Partly cloudy" },
+  3: { icon: "/Weather-website/images/icon-overcast.webp", label: "Overcast" },
+  45: { icon: "/Weather-website/images/icon-fog.webp", label: "Fog" },
+  48: { icon: "/Weather-website/images/icon-fog.webp", label: "Depositing rime fog" },
+  51: { icon: "/Weather-website/images/icon-rain.webp", label: "Light drizzle" },
+  61: { icon: "/Weather-website/images/icon-rain.webp", label: "Rain" },
+  71: { icon: "/Weather-website/images/icon-snow.webp", label: "Snow" },
+  95: { icon: "/Weather-website/images/icon-storm.webp", label: "Thunderstorm" },
 };
 
 export function getWeatherCondition(code: number | undefined) {
